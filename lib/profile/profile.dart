10eca85c7fd2f6ab791d2ef8106cd7779ebe98df
adil <adilsaid64@gmail.com>
2024-11-15 22:45:38 +0000

    feat: added anon signin and signout methods

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/services/auth.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: ElevatedButton(
+        child: const Text('SignOut'),
+        onPressed: () async {
+          await AuthService().signOut();
+          Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+        },
+      ),
+    );
   }
 }
