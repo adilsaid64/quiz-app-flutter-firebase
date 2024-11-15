c0f398dfd4ed1b6d1cfe4486e7dbfd7a158885d6
adil <adilsaid64@gmail.com>
2024-11-15 22:08:28 +0000

    feat: added anon login

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/login/login.dart';
+import 'package:quiz_app/services/auth.dart';
 import 'package:quiz_app/shared/bottom_nav.dart';
+import 'package:quiz_app/topics/topics.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavBar(),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('About'),
-          onPressed: () => Navigator.pushNamed(context, '/about'),
-        ),
-      ),
-    );
+    return StreamBuilder(
+        stream: AuthService().userStream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('Loading...');
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: Text('error'),
+            );
+          } else if (snapshot.hasData) {
+            return const TopicsScreen();
+          } else {
+            return const LoginScreen();
+          }
+        });
   }
 }
--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/shared/bottom_nav.dart';
 
 class TopicsScreen extends StatelessWidget {
   const TopicsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: BottomNavBar(),
+    );
   }
 }
