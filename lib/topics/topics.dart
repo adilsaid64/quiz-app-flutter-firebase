7a02eb0d26eb0a9e73189ff07a4d85683782fb36
adil <adilsaid64@gmail.com>
2024-11-16 20:48:22 +0000

    feat: added ability to fetchdata and display loading bar

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/services/firestore.dart';
+import 'package:quiz_app/services/models.dart';
 import 'package:quiz_app/shared/bottom_nav.dart';
+import 'package:quiz_app/shared/loading.dart';
 
 class TopicsScreen extends StatelessWidget {
   const TopicsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavBar(),
+    return FutureBuilder<List<Topic>>(
+      future: FirestoreService().getTopics(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingScreen();
+        } else if (snapshot.hasError) {
+          //
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.deepPurple,
+              title: const Text('Error'),
+            ),
+          );
+        } else if (snapshot.hasData) {
+          //
+          return Scaffold(
+            appBar: AppBar(
+                backgroundColor: Colors.deepPurple,
+                title: const Text('Topics')),
+          );
+        } else {
+          //
+          return Scaffold(
+            appBar: AppBar(
+                backgroundColor: Colors.deepPurple,
+                title: const Text('No Data')),
+          );
+        }
+      },
     );
   }
 }
