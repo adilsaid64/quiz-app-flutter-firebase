9ee7d28915d9635ba60725c5d1cc438ecc4e8f11
adil <adilsaid64@gmail.com>
2024-11-16 20:53:46 +0000

    feat: added error message widget

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/services/firestore.dart';
 import 'package:quiz_app/services/models.dart';
 import 'package:quiz_app/shared/bottom_nav.dart';
+import 'package:quiz_app/shared/error.dart';
 import 'package:quiz_app/shared/loading.dart';
 
 class TopicsScreen extends StatelessWidget {
@@ -15,11 +16,9 @@ class TopicsScreen extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const LoadingScreen();
         } else if (snapshot.hasError) {
-          //
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.deepPurple,
-              title: const Text('Error'),
+          return Center(
+            child: ErrorMessage(
+              message: snapshot.error.toString(),
             ),
           );
         } else if (snapshot.hasData) {
