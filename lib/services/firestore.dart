315af1422c04305b231f9f41f13e00276729408d
adil <adilsaid64@gmail.com>
2024-11-16 19:41:11 +0000

    feat: added logic to update  after user has compelted a quiz

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -23,7 +23,6 @@ class FirestoreService {
     return Quiz.fromJson(snapshot.data() ?? {});
   }
 
-
   // Listning to a real time stream of user reports. This reports section updates as users completed quizes.
   Stream<Report> streamReport() {
     // listen for the current signed in user id
@@ -35,5 +34,21 @@ class FirestoreService {
         return Stream.fromIterable([Report()]);
       }
     });
+  }
+
+  // Method to update report document after completing a quiz
+  Future<void> updateUserReport(Quiz quiz) {
+    var user = AuthService().user!;
+    var ref = _db.collection('reports').doc(user.uid);
+
+    var data = {
+      'total': FieldValue.increment(1), // add one to the existing count
+      'topics': {
+        quiz.topic: FieldValue.arrayUnion([quiz.id])
+      }
+    };
+
+    return ref.set(
+        data, SetOptions(merge: true)); // merge is true to not overwrite
   }
 }
